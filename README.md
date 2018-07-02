69b355f5560d6ce20c8b67a82987c2b595cf6ff5
alexen2 <psany@yandex.ru>
2018-07-02 09:55:33 +0300

    home fix

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ HTML template
 Home
 https://alexen2.github.io/lovimtut/
 
+Карточка товара
+https://alexen2.github.io/lovimtut/02_product-page.html
+
 04 Отзывы
 https://alexen2.github.io/lovimtut/04_testimonials.html
 
--- build/js/script.js
+++ build/js/script.js
@@ -129,6 +129,31 @@ $(function() {
 });
 
 $(document).ready(function () {
+  var swiper = new Swiper('.slider-overview__body', {
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    spaceBetween: 25,
+
+    navigation: {
+      nextEl: '.slider-overview__next',
+      prevEl: '.slider-overview__prev',
+    },
+
+    breakpoints: {   
+      1199: {
+        spaceBetween: 350,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      1200: {
+        spaceBetween: 54,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      }
+    }
+  });    
+});
+$(document).ready(function () {
   var swiper = new Swiper('.slider-top__body', {
     slidesPerView: 3,
     slidesPerGroup: 3,
