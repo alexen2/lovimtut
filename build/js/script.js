e29d3f6fa43380a4f78bc56c0d3f2c05d2e8c03a
alexen2 <psany@yandex.ru>
2018-07-05 16:17:19 +0300

    add cat filter

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,56 @@
+$(document).ready(function() {	
+/*	$('.footer__ico-money img').hover(function() {
+		$(this).attr('src', "images/" + $(this).attr('data-color') + "-color.svg");
+	}, function() {
+		$(this).attr('src', "images/" + $(this).attr('data-color') + ".svg");
+	});*/
+
+	jQuery("#slider-1").slider({
+		min: 0,
+		max: 35000,
+		values: [698, 29698],
+		range: true,
+		stop: function(event, ui) {
+			jQuery("input#minCost-1").val(jQuery("#slider-1").slider("values",0));
+			jQuery("input#maxCost-1").val(jQuery("#slider-1").slider("values",1));
+	  },
+	  slide: function(event, ui){
+			jQuery("input#minCost-1").val(jQuery("#slider-1").slider("values",0));
+			jQuery("input#maxCost-1").val(jQuery("#slider-1").slider("values",1));
+	  }
+	});
+
+	jQuery("#slider-2").slider({
+		min: 0,
+		max: 7000,
+		values: [963, 2987],
+		range: true,
+		stop: function(event, ui) {
+			jQuery("input#minCost-2").val(jQuery("#slider-2").slider("values",0));
+			jQuery("input#maxCost-2").val(jQuery("#slider-2").slider("values",1));
+	  },
+	  slide: function(event, ui){
+			jQuery("input#minCost-2").val(jQuery("#slider-2").slider("values",0));
+			jQuery("input#maxCost-2").val(jQuery("#slider-2").slider("values",1));
+	  }
+	});
+
+	jQuery("#slider-3").slider({
+		min: 0,
+		max: 7000,
+		step: 200,
+		values: [800, 5000],
+		range: true,
+		stop: function(event, ui) {
+			jQuery("input#minCost-3").val(jQuery("#slider-3").slider("values",0));
+			jQuery("input#maxCost-3").val(jQuery("#slider-3").slider("values",1));
+	  },
+	  slide: function(event, ui){
+			jQuery("input#minCost-3").val(jQuery("#slider-3").slider("values",0));
+			jQuery("input#maxCost-3").val(jQuery("#slider-3").slider("values",1));
+	  }
+	});
+});
 $(function() {
 	$('#cat-sort').selectize({
   	create: true,
