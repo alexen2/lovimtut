3e3266aab38c13dcd394b234eb4f158735fdc156
alexen2 <psany@yandex.ru>
2018-07-07 10:25:58 +0300

    fix filter mobile

--- build/js/script.js
+++ build/js/script.js
@@ -525,13 +525,16 @@ $(document).ready(function() {
 
 		if(mobile_filter.hasClass('mobile-filter--show')) {
 			mobile_filter.removeClass('mobile-filter--show');
+			$("html,body").css("overflow-y", "auto");
 		} else {
 			mobile_filter.addClass('mobile-filter--show');
+			$("html,body").css("overflow-y", "hidden");
 		}		
 	});
 
 	$('.mobile-filter-close').click(function() {
 		$('.mobile-filter').removeClass('mobile-filter--show');
+		$("html,body").css("overflow-y", "auto");
 	});
 });
 $(function() {
--- src/blocks/mobile-filter/mobile-filter.js
+++ src/blocks/mobile-filter/mobile-filter.js
@@ -6,12 +6,15 @@ $(document).ready(function() {
 
 		if(mobile_filter.hasClass('mobile-filter--show')) {
 			mobile_filter.removeClass('mobile-filter--show');
+			$("html,body").css("overflow-y", "auto");
 		} else {
 			mobile_filter.addClass('mobile-filter--show');
+			$("html,body").css("overflow-y", "hidden");
 		}		
 	});
 
 	$('.mobile-filter-close').click(function() {
 		$('.mobile-filter').removeClass('mobile-filter--show');
+		$("html,body").css("overflow-y", "auto");
 	});
 });
