2178fcd0e8e8e8824b37c9e5cb464a527389724b
alexen2 <psany@yandex.ru>
2018-07-11 08:53:07 +0300

    product-page

--- js/script.js
+++ js/script.js
@@ -574,6 +574,52 @@ $(document).ready(function () {
     }
   });    
 });
+$(document).ready(function() {
+	var swiper = new Swiper('.slider-product__slider', {
+    direction: 'vertical',    
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    mousewheel: true,
+
+    pagination: {
+      el: '.slider-product__pag',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.slider-product__next',
+      prevEl: '.slider-product__prev',
+    },
+
+    breakpoints: {   
+      767: {
+        direction: 'horizontal',
+        spaceBetween: 50,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      1199: {
+        direction: 'vertical',
+        spaceBetween: 20,
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+      },
+      1719: {
+        direction: 'horizontal',
+        spaceBetween: 15,
+        slidesPerView: 3,
+        slidesPerGroup: 1,
+      },
+      1720: {
+        direction: 'vertical',
+        spaceBetween: 20,
+        slidesPerView: 3,
+        slidesPerGroup: 1,
+      }
+    } 
+  });
+});
 $(document).ready(function () {
   var swiper = new Swiper('.slider-top__body', {
     slidesPerView: 3,
